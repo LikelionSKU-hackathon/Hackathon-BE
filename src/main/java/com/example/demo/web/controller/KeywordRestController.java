87ca8ab968d2bd37239051cd88c1795c632d1fbb
Kim So Yeon <qws1566@naver.com>
2024-07-30 20:33:58 +0900

    :bug: fix: 키워드 관련 로직 수정

--- src/main/java/com/example/demo/web/controller/KeywordRestController.java
+++ src/main/java/com/example/demo/web/controller/KeywordRestController.java
@@ -9,6 +9,7 @@ import com.example.demo.service.MemberService.MemberQueryService;
 import com.example.demo.web.dto.MemberRequestDTO;
 import com.example.demo.web.dto.MemberResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -21,19 +22,18 @@ public class
 KeywordRestController {
     private final MemberCommandService memberCommandService;
     private final MemberQueryService memberQueryService;
-    @GetMapping("/")
+    @GetMapping("/{memberId}")
     @Operation(summary="키워드 목록 조회API", description="회원의 연령별 키워드 목록을 조회하는 API")
-    public ApiResponse<MemberResponseDTO.KeywordResultDTO> keyword(Authentication authentication) {
-        Long memberId = (Long) authentication.getPrincipal();
+    public ApiResponse<MemberResponseDTO.KeywordResultDTO> keyword(@PathVariable(name="memberId") Long memberId) {
         List<Keyword> keywords = memberQueryService.getKeyword(memberId);
         return ApiResponse.onSuccess(MemberConverter.toKeywordResultDTO(memberId, keywords));
     }
-    @PostMapping("/")
+    @PostMapping("/{memberId}")
     @Operation(summary="키워드 선택 API", description="회원의 키워드 선택 API")
     public ApiResponse<MemberResponseDTO.setKeywordResultDTO> setKeyword(
-            @RequestBody MemberRequestDTO.setKeywordDTO request,
-            Authentication authentication) {
-        Long memberId = (Long) authentication.getPrincipal();
+            @RequestBody @Valid MemberRequestDTO.setKeywordDTO request,
+            @PathVariable(name="memberId") Long memberId) {
+
         Member member = memberCommandService.setKeyword(request, memberId);
         return ApiResponse.onSuccess(MemberConverter.toSetKeywordResultDTO(member));
     }
